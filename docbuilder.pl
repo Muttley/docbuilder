731094a209bf6827cc4077693c232edb1acc0758
Paul Maskelyne (Muttley) <muttley@muttleyville.org>
2014-02-04 17:10:05 +0000

    code tidy

--- docbuilder.pl
+++ docbuilder.pl
@@ -7,6 +7,7 @@ use File::Basename;
 use FindBin;
 use Getopt::Long;
 use JSON::XS;
+use Path::Class;
 
 use lib "$FindBin::Bin/lib";
 
@@ -23,12 +24,19 @@ my $options = {
 };
 
 sub bootstrap {
-	my $directory = $options->{output};
+	my $base_dir = dir ($options->{output});
 
-	my $dirname = $directory ne '.' ? $directory : 'current directory';
+	my $dirname = "$base_dir" ne '.' ? "$base_dir" : 'current directory';
+
 	say "Bootstrapping document in $dirname...";
 
-	for my $dir ($directory, "$directory/en", "$directory/images") {
+	my @dirs = (
+		$base_dir,
+		dir ($base_dir, 'en'),
+		dir ($base_dir, 'images')
+	);
+
+	for my $dir (@dirs) {
 		unless (-d $dir) {
 			mkdir $dir
 				|| die "Unable to create output directory: $!";
@@ -37,57 +45,48 @@ sub bootstrap {
 
 	my $json_encoder = JSON::XS->new->ascii->pretty->allow_nonref;
 
-	my $config = $json_encoder->encode ({
-		copyright => (localtime)[5] + 1900,
-		index => JSON::XS::false,
-		title => 'Untitled Document',
-		version => undef,
-	});
+	write_data_to_file(
+		file ($base_dir, 'config.json'),
+		$json_encoder->encode({
+			copyright => (localtime)[5] + 1900,
+			index     => JSON::XS::false,
+			title     => 'Untitled Document',
+			version   => undef,
+		})
+	);
 
-	open my $fh, '>', "$directory/config.json"
-		|| die "Unable to open file: $!";
-	print $fh $config;
-	close $fh;
+	write_data_to_file(
+		file ($base_dir, 'replacements.json'),
+		$json_encoder->encode({
+			global => {
+				___example___ => "Example Replacement",
+			}
+		})
+	);
 
-	my $replacements = $json_encoder->encode ({
-		"global" => {
-			"___example___" => "Example Replacement",
-		}
-	});
-
-	open my $fh, '>', "$directory/replacements.json"
-		|| die "Unable to open file: $!";
-	print $fh $replacements;
-	close $fh;
-
-	my $md = "# Chapter 1\n\nThis is the first chapter.\n";
-
-	open my $fh, '>', "$directory/en/01.00-Chapter1.md"
-		|| die "Unable to open file: $!";
-	print $fh $md;
-	close $fh;
-}
-
-sub handle_options {
-	GetOptions(
-		'bootstrap|b'  => \$options->{bootstrap},
-		'config|c=s'   => $options->{config},
-		'language|l=s' => \$options->{language},
-		'output|o=s'   => \$options->{output},
-		'source|s=s'   => \$options->{source},
-		'help|usage|?' => sub { usage(); },
+	write_data_to_file(
+		file ($base_dir, 'en', '01.00-Chapter1.md'),
+		"# Chapter 1\n\nThis is the first chapter.\n"
 	);
 }
 
 sub main {
-	handle_options();
+	GetOptions(
+		'bootstrap|b'  => \$options->{bootstrap},
+		'config|c=s'   => \$options->{config},
+		'language|l=s' => \$options->{language},
+		'output|o=s'   => \$options->{output},
+		'source|s=s'   => \$options->{source},
+
+		'help|usage|?' => sub { usage(); }
+	);
 
 	if ($options->{bootstrap}) {
-		bootstrap();
+		bootstrap;
 		exit;
 	}
 
-	my $docbuilder = Mutt::DocBuilder->new (
+	my $docbuilder = Mutt::DocBuilder->new(
 		language => $options->{language},
 		output   => $options->{output},
 		source   => $options->{source}
@@ -105,38 +104,54 @@ sub usage {
 	print qq(
 $program
 
-    Processes a manual's source Markdown text files into a single PDF. These
-    files must have a .md file extension.
+    Processes a directory of source Markdown into a single PDF. These files
+    must have a .md file extension.
+
+    See the full documentation for details on additionally supported markup
+    tags for inserting various document elements.
 
 options:
 
-    --b|bootstrap   Bootstrap a new document project.  This creates a base
-                    document project in the directory specified by the
-                    --o|output option.
+    -b|bootstrap   Bootstrap a new document project.  This creates a base
+                   document project in the directory specified by the
+                   -o|output option.
 
-    --c|config      Additional config options in key=value format. These
-                    options will override any with the same name in the
-                    document's config.json file.  Can be specified mutliple
-                    times.
+    -c|config      Additional config options in key=value format. These
+                   options will override any with the same name in the
+                   document's config.json file.  Can be specified mutliple
+                   times.
 
-    --l|language    Language of the manual to be built. The language
-                    sub-directory will automatically be appended to the
-                    source directory. (default: "en")
+    -l|language    Language of the manual to be built. The language
+                   sub-directory will automatically be appended to the
+                   source directory. (default: "en")
 
-    --o|output      The output directory where the final PDF should be
-                    created (default: ".")
+    -o|output      The output directory where the final PDF should be
+                   created (default: ".")
 
-    --s|source      Base manual source directory.
-                    (default: "./manual")
+    -s|source      Base manual source directory.
+                   (default: "./manual")
 
 usage:
 
     $program
-    $program --l de --s ./manual --o /opt/documents
-    $program --l fr --s ./manual
+    $program -l de -s ./manual -o /opt/documents
+    $program -l en -s . -c version=1.0.2
+    $program -l fr -s ./manual
 
 );
 	exit 1;
 }
 
-main();
+sub write_data_to_file {
+	my $file = shift;
+	my $data = shift;
+
+	open my $fh, '>', "$file"
+		or die "Unable to open file '$file': $!";
+
+	print $fh $data;
+
+	close $fh;
+}
+
+main;
